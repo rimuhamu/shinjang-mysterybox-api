779415d1f2d42f1616bc0c3bb4ef910d2aad263f
Emir Risyad <emirrsyd@gmail.com>
2024-03-19 22:44:14 +0100

    initial commit

---
+++ src/index.ts
@@ -0,0 +1,22 @@
+import express from 'express';
+import http from 'http';
+import bodyParser from 'body-parser';
+import cookieParser from 'cookie-parser';
+import compression from 'compression';
+import cors from 'cors';
+
+const app = express();
+
+app.use(
+  cors({
+    credentials: true,
+  })
+);
+
+app.use(compression());
+app.use(cookieParser());
+app.use(bodyParser.json());
+
+app.listen(3000, () => {
+  console.log('Server running on port 3000');
+});
